0112dd82d7f2b96b06c3ed02219e12eab06e57aa
Cbodtorf <cbodtorf@gmail.com>
2016-09-22 18:01:22 -0400

    basic node/hapi server running on port 8332. Which is the closest numerical value to BEER. I added 3 basic routes based on you api, but not the specific modules. Added a gitignore for the node_modules, so you may need to install the added dependencies. (hapi inert boom).

---
+++ src/server.js
@@ -0,0 +1,72 @@
+/***********************
+* Node backed w/ Hapi
+***********************/
+'use strict'
+const Hapi = require('hapi')
+const Boom = require ('boom')
+const Inert = require('inert')
+
+
+const server = new Hapi.Server()
+server.connection({
+  host: 'localhost',
+  port: 8332
+})
+
+
+/***********************
+*  ROUTES
+***********************/
+server.route({
+  method: 'GET',
+  path: '/accounts',
+
+  handler(request, reply) {
+    reply(console.log(' account placeholder'))
+  }
+})
+
+server.route({
+  method: 'GET',
+  path: '/inventory',
+
+  handler(request, reply) {
+    reply(console.log(' inventory placeholder'))
+  }
+})
+
+server.route({
+  method: 'GET',
+  path: '/users',
+
+  handler(request, reply) {
+    reply(console.log(' users placeholder'))
+  }
+})
+
+
+/***********************
+*  INERT
+* --- *
+* Static serving of files. Web requests are all GET requests, and the
+* path is basically saying "anything" here. Retrieve the specified files
+* from the public/ directory (where your gulpfile should be outputting
+* to) and return them.
+***********************/
+server.route({
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: '../public/',
+      redirectToSlash: true,
+      index: true
+    }
+  }
+});
+
+server.start((err) => {
+  if (error) { throw err }
+
+  console.log('Server running at:', server.info.uri)
+})
